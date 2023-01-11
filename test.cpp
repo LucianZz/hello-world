494f436c54f7133f1b0ae5d6f1aae4a4e6964177
LucianZz <87650147+LucianZz@users.noreply.github.com>
2023-01-11 11:02:05 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,59 @@
+void foo() {
+    int array[10];
+    int i = get();
+    // i = 9;
+    if (i > 8 && i <= length(array)) {  // Shoud be i < length(array)
+        array[i] = 1;  // defect: array[10] overflow
+    }
+    array[i] = 1;  // defect: array[10] overflow
+}
+
+
+void test(int i) {
+    int n= 10;
+    char *p = malloc(sizeof(int) * 10);
+    int y = n;
+    p[y] = 'a'; // defect: writing to buffer[y] overflow
+}
+
+void increase1() {
+    while (1) {
+        mtx.lock();
+        if (counter <= 1000)
+            counter++;
+        else
+            break;
+        read_counter(counter);  // defect: dead_lock
+        mtx.unlock()
+    }
+}
+void read_counter(counter){
+    std::cout << counter << std::endl;
+    do_something_more();
+}
+void increase1() {
+    while (1) {
+        std::lock_guard<std::mutex> lk(mtx);  // good: 使用lock_guard会自动上锁解锁将不会检查dead_lock
+        if (counter <= 1000)
+            counter++;
+        else
+            break;
+        read_counter(counter);
+    }
+}
+
+const char* key_value(const int key) {
+    const char * value = 0;
+    if (key != 0) {
+        value = "Zeus";
+    } else if (key != 1) {
+        value = "Hades";
+    } 
+    if (key != 2) { // Should be 'else if' here.
+        value = "Poseidon";  // defect: unused_value Zeus Hades  never used
+    }
+    else {
+        value = "Unknow
+    }
+    return result;
+}
